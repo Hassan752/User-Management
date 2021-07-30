d4daf16c843d08ccb85389915d24f89de2dc5de0
Hassan752 <66700578+Hassan752@users.noreply.github.com>
2021-07-30 22:01:37 +0200

    Create model.js

---
+++ server/model/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : true
+
+    },
+    email:{
+        type : String,
+        required : true,
+        unique : true
+
+    },
+    gender:{
+        
+    },
+    
+    status:{
+        type : String,
+
+    }
+})
+
+const Userdb = mongoose.model('userdb' , schema);
+
+module.exports = Userdb;
